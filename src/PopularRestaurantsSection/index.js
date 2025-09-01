60795d64428c8cd61e0e383b52866237f483cef5
raju <rajukunchla321@gmail>
2025-09-01 18:28:08 +0530

    popular rest updated

--- src/PopularRestaurantsSection/index.js
+++ src/PopularRestaurantsSection/index.js
@@ -115,7 +115,7 @@ const Index = () => {
            <div className="pagination-container"><button onClick={handlePageDecrement} className="arrow-btn"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path fillRule="evenodd" clipRule="evenodd" d="M9.87352 2L11 3.15074L6.25296 8L11 12.8493L9.87352 14L4.68479 8.69953C4.30425 8.3108 4.30425 7.68919 4.68479 7.30046L9.87352 2Z" fill="#334155"/>
 </svg>
-</button><div>{activePage} of 20</div><button onClick={handlePageIncrement} className="arrow-btn"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+</button><div>{activePage} of 4</div><button onClick={handlePageIncrement} className="arrow-btn"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path fillRule="evenodd" clipRule="evenodd" d="M6.12648 14L5 12.8493L9.74704 8L5 3.15074L6.12648 2L11.3152 7.30047C11.6957 7.6892 11.6957 8.31081 11.3152 8.69954L6.12648 14Z" fill="#334155"/>
 </svg>
 </button></div>
